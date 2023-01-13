5bee6b88d8de8e3163058eb7f9066b583f0b830d
Draq109 <daniel.vazquez03@utrgv.edu>
2023-01-13 17:30:26 -0600

    Create header with three tabs, add font

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import './style.css'
+
+function createHeader(className) {
+    const element = document.createElement('header');
+    element.setAttribute('class', className);
+    return element;
+}
+
+function createHeaderTab(textContent, className) {
+    const element = document.createElement('h1');
+    element.innerHTML = textContent;
+    element.setAttribute('class', className);
+
+    return element;
+}
+
+let content = document.getElementById('content');
+content.appendChild(createHeader('header'));
+
+let header = document.getElementsByClassName('header');
+header[0].appendChild(createHeaderTab('Home', 'home'));
+header[0].appendChild(createHeaderTab('Menu', 'menu'));
+header[0].appendChild(createHeaderTab('Contact', 'contact'));
+
+
+
+
